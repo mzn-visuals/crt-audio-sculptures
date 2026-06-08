bb7467cd469e7ea3c0518406489fdb4895d0045b
mmaznnn <94003574+mzn-visuals@users.noreply.github.com>
2026-06-08 13:43:35 +0400

    Add files via upload

--- proxy.js
+++ proxy.js
@@ -23,6 +23,16 @@
  *   Range requests (for audio streaming) are forwarded transparently.
  */
 
+const fs = require('fs');
+
+// Debug: check cookies file
+const cookiesPath = '/etc/secrets/cookies.txt';
+if (fs.existsSync(cookiesPath)) {
+  const content = fs.readFileSync(cookiesPath, 'utf8');
+  console.log('[cookies] file found, first line:', content.split('\n')[0]);
+} else {
+  console.log('[cookies] FILE NOT FOUND at', cookiesPath);
+}
 const http        = require("http");
 const https       = require("https");
 const { URL }     = require("url");
@@ -87,7 +97,7 @@ const promise = new Promise((resolve, reject) => {
     const ytUrl = `https://www.youtube.com/watch?v=${videoId}`;
     execFile(
     "yt-dlp",
-    ["--no-playlist", "-f", "bestaudio", "--get-url", "--extractor-args", "youtube:player_client=ios", ytUrl],
+    ["--cookies", "/etc/secrets/cookies.txt", "--no-playlist", "-f", "bestaudio[ext=webm]/bestaudio[ext=m4a]/bestaudio", "--get-url", ytUrl],
     { timeout: 20000 },
       (err, stdout, stderr) => {
         streamInFlight.delete(videoId);
